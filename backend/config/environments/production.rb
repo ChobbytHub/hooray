df0b2214ccbd1b1eb7fcc66c8e4f66e19049e507
chobby <aa.ing85@gmail.com>
2025-11-05 17:20:34 +0900

    Configure production host for Render deployment and allow mini-report.onrender.com

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -77,12 +77,12 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
+  config.hosts = [
+    "mini-report.onrender.com"
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts << "mini-report.onrender.com"
 end
