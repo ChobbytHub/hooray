ee84acdb58dc82d978a751f9ee68b7cf6bbc9c81
chobby <aa.ing85@gmail.com>
2025-11-07 17:00:26 +0900

    Add jwt_denylist

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_07_015557) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_07_075158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,14 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_07_015557) do
     t.index ["post_id"], name: "index_hoorays_on_post_id"
     t.index ["user_id", "post_id"], name: "index_hoorays_on_user_id_and_post_id", unique: true
     t.index ["user_id"], name: "index_hoorays_on_user_id"
+  end
+
+  create_table "jwt_denylists", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "exp"
+    t.string "jti"
+    t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylists_on_jti"
   end
 
   create_table "posts", force: :cascade do |t|
